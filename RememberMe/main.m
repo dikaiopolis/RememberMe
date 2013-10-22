bda6a35b295ac26e60e87e3818c89d84dbccdd9f
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-10-22 11:41:58 -0500

    Xcode template

---
+++ RememberMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RememberMe
+//
+//  Created by Daniel Baldwin on 10/22/13.
+//  Copyright (c) 2013 Daniel Baldwin & Ibrahim Ayad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
