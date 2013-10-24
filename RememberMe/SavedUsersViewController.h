71a1985019ea3e0ced9517493421eb42b20f0ea4
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-10-24 11:39:46 -0500

    created UserProfileViewController

---
+++ RememberMe/SavedUsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedUsersViewController.h
+//  RememberMe
+//
+//  Created by Daniel Baldwin on 10/24/13.
+//  Copyright (c) 2013 Daniel Baldwin & Ibrahim Ayad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedUsersViewController : UIViewController
+
+@end
---
+++ RememberMe/WhoIsNearMeViewController.m
@@ -0,0 +1,38 @@
+//
+//  WhoIsNearMeViewController.m
+//  RememberMe
+//
+//  Created by Daniel Baldwin on 10/24/13.
+//  Copyright (c) 2013 Daniel Baldwin & Ibrahim Ayad. All rights reserved.
+//
+
+#import "WhoIsNearMeViewController.h"
+
+@interface WhoIsNearMeViewController ()
+
+@end
+
+@implementation WhoIsNearMeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
